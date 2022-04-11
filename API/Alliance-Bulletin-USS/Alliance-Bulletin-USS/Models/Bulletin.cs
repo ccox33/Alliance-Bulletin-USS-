b5f21073955f03d373d0408aca0b7a41a50cfb7b
jdouthitt2 <89221167+jdouthitt2@users.noreply.github.com>
2022-04-10 23:27:45 -0500

    API Backend Base
    The basic API Backend with some basic routes to connect to the Bulletin database.

---
+++ API/Alliance-Bulletin-USS/Alliance-Bulletin-USS/Models/Bulletin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alliance_Bulletin_USS.Models
+{
+    public class Bulletin
+    {
+        public int identity { get; set; }
+
+        public int createdDay { get; set; }
+        public int createdMonth { get; set; }
+        public int createdYear { get; set; }
+
+        public string dateCreated { get; set; }
+        public string topic { get; set; }
+        public string software { get; set; }
+        public string symptom { get; set; }
+        public string resolution { get; set; }
+        public string notes { get; set; }
+        public string noteImage { get; set; }
+
+        public bool isDeleted { get; set; }
+
+        public int modifiedDay { get; set; }
+        public int modifiedMonth { get; set; }
+        public int modifiedYear { get; set; }
+
+        
+    }
+}

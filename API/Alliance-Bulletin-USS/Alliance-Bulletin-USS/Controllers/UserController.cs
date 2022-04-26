8568644a740428b2e7ec74824dab46e2f9daee60
jdouthitt2 <89221167+jdouthitt2@users.noreply.github.com>
2022-04-26 15:15:59 -0500

    Identity Framework Part 2

--- API/Alliance-Bulletin-USS/Alliance-Bulletin-USS/Controllers/UserController.cs
+++ API/Alliance-Bulletin-USS/Alliance-Bulletin-USS/Controllers/UserController.cs
@@ -2,9 +2,13 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Alliance_Bulletin_USS.Controllers
@@ -15,12 +19,34 @@ namespace Alliance_Bulletin_USS.Controllers
     public class UserController : ControllerBase
     {
 
+        [AllowAnonymous]
         [HttpPost("getToken")]
         public IActionResult GetToken(User user)
         {
 
-            var tokenString = "";
-            return Ok(new {Token = tokenString});
+            if (true)
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes("THIS_KEY_WORKS_FINE_137258021847124435");
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                    new Claim(ClaimTypes.Name, user.UserEmail)
+                    }),
+                    Expires = DateTime.UtcNow.AddDays(1),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var tokenString = tokenHandler.WriteToken(token);
+
+                return Ok(new { Token = tokenString });
+            }
+            else
+            {
+                return Unauthorized("Try Again!");
+            }
+            
         }
 
     }
--- API/Alliance-Bulletin-USS/Alliance-Bulletin-USS/Models/User.cs
+++ API/Alliance-Bulletin-USS/Alliance-Bulletin-USS/Models/User.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Alliance_Bulletin_USS.Models
 {
